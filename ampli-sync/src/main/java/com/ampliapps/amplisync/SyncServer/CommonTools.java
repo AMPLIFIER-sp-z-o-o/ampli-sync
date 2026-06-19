88ffa3810d63306dfec703b69849c2e1a433bbcd
Marcin Lisowski <mnlisows@gmail.com>
2026-06-19 05:06:14 +0200

    feat: sync local demo tables

--- ampli-sync/src/main/java/com/ampliapps/amplisync/SyncServer/CommonTools.java
+++ ampli-sync/src/main/java/com/ampliapps/amplisync/SyncServer/CommonTools.java
@@ -384,10 +384,13 @@ public class CommonTools {
     }
 
     public void InitSync(String schema){
-        // this is placeholder
-        // you should initialize here every table you want synchronize
         AddTableToSynchronization(schema, "document_headers");
+        AddTableToSynchronization(schema, "demo_customers");
+        AddTableToSynchronization(schema, "demo_products");
+        AddTableToSynchronization(schema, "demo_orders");
+        AddTableToSynchronization(schema, "demo_order_items");
     }
+
 
     public void AddFileToZip(File zipFile, File fileToAdd) {
         try {
