fc2e3222c1b6960dba44aff84ec3b430d282bb39
Marcin Lisowski <mnlisows@gmail.com>
2026-06-22 10:10:49 +0200

    feat: add dev client skeleton, java 21 -> 17

---
+++ tools/dev-client/src/main/java/com/ampliapps/amplisync/devclient/SyncDevClient.java
@@ -0,0 +1,13 @@
+package com.ampliapps.amplisync.devclient;
+
+public class SyncDevClient {
+    /*
+     - call backend health endpoint
+     - download, unpack sqlite database from prepopulate-db
+     - open sqlite database
+     - perform local insert/update/delete operations
+     - build payload from local SQLite, like in rn client
+     - send local changes to the backend
+     - pull changes from the backend
+     */
+}
