b3f939ef76ee79c1c21a5cbb1b68325db0113b9b
Marcin Lisowski <mnlisows@gmail.com>
2026-07-29 09:52:27 +0200

    fix: namespace sync data files by schema

--- ampli-sync/src/main/java/com/ampliapps/amplisync/SyncServer/Synchronization/SyncSessionStore.java
+++ ampli-sync/src/main/java/com/ampliapps/amplisync/SyncServer/Synchronization/SyncSessionStore.java
@@ -1,43 +1,57 @@
 package com.ampliapps.amplisync.SyncServer.Synchronization;
 
 import com.ampliapps.amplisync.SQLiteSyncConfig;
+import java.io.File;
+
 
 import javax.sql.rowset.CachedRowSet;
 
 final class SyncSessionStore {
 
-    void writeSyncData(String syncId, CachedRowSet inserts, CachedRowSet updates, CachedRowSet deletes) {
+    void writeSyncData(String schema, String syncId, CachedRowSet inserts, CachedRowSet updates, CachedRowSet deletes) {
+
+        ensureSyncDataDirectoryExists(schema);
+
         BinaryWriter binaryWriter = binaryWriter();
-        binaryWriter.writeToBinary(syncDataFile(syncId), inserts);
-        binaryWriter.writeToBinary(syncDataUpdatesFile(syncId), updates);
-        binaryWriter.writeToBinary(syncDataDeletesFile(syncId), deletes);
+        binaryWriter.writeToBinary(syncDataFile(schema, syncId), inserts);
+        binaryWriter.writeToBinary(syncDataUpdatesFile(schema, syncId), updates);
+        binaryWriter.writeToBinary(syncDataDeletesFile(schema, syncId), deletes);
+
     }
 
-    CachedRowSet readInserts(String syncId) {
-        return (CachedRowSet) binaryWriter().readFromBinaryFile(syncDataFile(syncId));
+    CachedRowSet readInserts(String syncId, String schema) {
+        return (CachedRowSet) binaryWriter().readFromBinaryFile(syncDataFile(schema, syncId));
     }
 
-    CachedRowSet readUpdates(String syncId) {
-        return (CachedRowSet) binaryWriter().readFromBinaryFile(syncDataUpdatesFile(syncId));
+    CachedRowSet readUpdates(String syncId, String schema) {
+        return (CachedRowSet) binaryWriter().readFromBinaryFile(syncDataUpdatesFile(schema, syncId));
     }
 
-    CachedRowSet readDeletes(String syncId) {
-        return (CachedRowSet) binaryWriter().readFromBinaryFile(syncDataDeletesFile(syncId));
+    CachedRowSet readDeletes(String syncId, String schema) {
+        return (CachedRowSet) binaryWriter().readFromBinaryFile(syncDataDeletesFile(schema, syncId));
     }
 
     private BinaryWriter binaryWriter() {
         return new BinaryWriter();
     }
 
-    private String syncDataFile(String syncId) {
-        return SQLiteSyncConfig.WORKING_DIR + "SyncData/" + syncId + ".dat";
+    private void ensureSyncDataDirectoryExists(String schema) {
+        File directory = new File(SQLiteSyncConfig.WORKING_DIR + "SyncData/" + schema);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
     }
 
-    private String syncDataUpdatesFile(String syncId) {
-        return SQLiteSyncConfig.WORKING_DIR + "SyncData/" + syncId + "_updates.dat";
+    private String syncDataFile(String schema, String syncId) {
+        return SQLiteSyncConfig.WORKING_DIR + "SyncData/" + schema + "/" + syncId + ".dat";
     }
 
-    private String syncDataDeletesFile(String syncId) {
-        return SQLiteSyncConfig.WORKING_DIR + "SyncData/" + syncId + "_deletes.dat";
+    private String syncDataUpdatesFile(String schema, String syncId) {
+        return SQLiteSyncConfig.WORKING_DIR + "SyncData/" + schema + "/" + syncId + "_updates.dat";
     }
+
+    private String syncDataDeletesFile(String schema, String syncId) {
+        return SQLiteSyncConfig.WORKING_DIR + "SyncData/" + schema + "/" + syncId + "_deletes.dat";
+    }
+
 }
